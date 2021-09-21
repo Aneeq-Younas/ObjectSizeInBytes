1cdd17221109973eee571da4c6e8eebece87e3aa
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-21 01:06:43 -0700

    Add files via upload

---
+++ bytsze.py
@@ -0,0 +1,30 @@
+import sys
+
+x1= "Aneeq Younas "
+x2= "John "
+a1= 3212
+a2= 4323
+x3= "David "
+x4= "James "
+a3= 6462
+a4= 97885
+x5= "Lucas"
+x6= "William "
+a5= 89327
+x7= "Noah "
+a6= 44
+a7= 36
+
+print("The size of a ", x1, "is: \n  ", str(sys.getsizeof(x1)) + " bytes ")
+print("The size of a ", x2, "is: \n  ", str(sys.getsizeof(x2)) + " bytes ")
+print("The size of a ", a1, "is: \n  ", str(sys.getsizeof(a1)) + " bytes ")
+print("The size of a ", a2, "is: \n  ", str(sys.getsizeof(a2)) + " bytes ")
+print("The size of a ", x3, "is: \n  ", str(sys.getsizeof(x3)) + " bytes ")
+print("The size of a ", x4, "is: \n  ", str(sys.getsizeof(x4)) + " bytes ")
+print("The size of a ", a3, "is: \n  ", str(sys.getsizeof(a3)) + " bytes ")
+print("The size of a ", a4, "is: \n  ", str(sys.getsizeof(a4)) + " bytes ")
+print("The size of a ", x5, "is: \n  ", str(sys.getsizeof(x5)) + " bytes ")
+print("The size of a ", x6, "is: \n  ", str(sys.getsizeof(x6)) + " bytes ")
+print("The size of a ", x7, "is: \n  ", str(sys.getsizeof(x7)) + " bytes ")
+print("The size of a ", a6, "is: \n  ", str(sys.getsizeof(a6)) + " bytes ")
+print("The size of a ", a7, "is: \n  ", str(sys.getsizeof(a7)) + " bytes ")
